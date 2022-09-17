4afa57eacf75b5b59c3bafbc7daf2d4e5d7f4cdd
Delta <dhriti.baruah20@gmail.com>
2022-09-17 16:46:38 +0530

    port console changed

--- app.js
+++ app.js
@@ -266,5 +266,5 @@ app.get('/deletequery/:id', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log(`Server started on ${PORT}`));
+app.listen(3000, () => console.log(`Server started on 3000`));
 
